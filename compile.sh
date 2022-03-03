86b75e4bc45967cfa6fbd79f3d0f7df00ab44874
Vassilis Kehayas <vkehayas@users.noreply.github.com>
2022-03-03 11:33:20 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ cd pkg/src
 rm --verbose *.o *.so
 gcc -std=gnu99 -I/usr/share/R/include -DNDEBUG -fpic -fopenmp -O2 -Wall -pipe  -g  -c *.c 
 #gcc -std=gnu99 -I/usr/share/R/include -DNDEBUG -fpic -O2 -Wall -pipe  -g  -c *.c 
-gcc -std=gnu99 -shared -o stringdist.so *.o -L/usr/lib/R/lib -lR
+gcc -std=gnu99 -shared -o stringdist.so *.o -L/usr/lib/R/lib -lR -lm
 cd ../../
 
